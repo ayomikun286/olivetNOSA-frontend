5187d9c2a2d66ad8834e1875ff9775cfc7da6657
ayomikun286 <edegbaiayomikun@gmail.com>
2026-09-06 04:33:38 +0100

    testing

--- src/components/public/Navbar.jsx
+++ src/components/public/Navbar.jsx
@@ -108,8 +108,8 @@ const Navbar = () => {
 
   return (
     <nav className={`fixed inset-x-0 top-0 z-50 transition-all duration-500 ${scrolled
-      ? "border-red-200"
-      : "border-red-200  "
+      ? "border-red-800"
+      : "border-red-800  "
       }`}>
 
       {/* =====================================================
