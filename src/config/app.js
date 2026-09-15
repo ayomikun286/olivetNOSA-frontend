87b4bf6d07c29b9a3c4733b9bb1239e79d94472b
ayomikun286 <edegbaiayomikun@gmail.com>
2026-09-15 02:53:53 +0100

    fix note drop

--- src/config/app.js
+++ src/config/app.js
@@ -1,4 +1,4 @@
 
-const API = "http://localhost:5000";
-// const API = 'https://api.olivetbhsnosa.org';
+// const API = "http://localhost:5000";
+const API = 'https://api.olivetbhsnosa.org';
 export default API;
